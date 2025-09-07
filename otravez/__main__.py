a16064a1f9559da2de3273ff40caa1242a2ad73c
Kaamkiya <ziam3.1415@gmail.com>
2025-09-07 08:52:49 -0400

    feat: Stat command works!!

--- otravez/__main__.py
+++ otravez/__main__.py
@@ -2,6 +2,7 @@ import os
 
 import discord
 import logging
+import tbapy
 from discord.ext import commands
 from dotenv import load_dotenv
 
@@ -13,7 +14,7 @@ intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 
-tba = tbapy.TBA(os.getenv("TBA_AUTH_KEY"))
+tba_client = tbapy.TBA(os.getenv("TBA_AUTH_KEY"))
 
 bot = commands.Bot(command_prefix=";", intents=intents)
 
@@ -40,7 +41,24 @@ async def ping(ctx):
     await ctx.send("pong")
 
 @bot.command()
-async def tba(ctx):
-    pass
+async def stat(ctx, *, args):
+    team_number, *args = args.split()
+
+    try:
+        int(team_number)
+    except ValueError:
+        await ctx.reply("Invalid team number. Usage: ;stat [teamnum]")
+
+    data = tba_client.team("frc" + team_number)
+
+    if data.get("nickname", None) is None:
+        await ctx.reply("No such team.")
+
+    await ctx.reply(f"""
+name: {data["nickname"]}
+location: {data["city"]}, {data["state_prov"] and f"{data["state_prov"]}, "}{data["country"]}
+school: {data["school_name"]}
+rookie year: {data["rookie_year"]}
+website: {data["website"]}""")
 
 bot.run(token, log_handler=handler, log_level=logging.DEBUG)
