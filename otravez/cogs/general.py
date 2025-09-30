6bf017bcafcae3dc407f2557c6d1ac85b2ec36cb
Kaamkiya <ziam3.1415@gmail.com>
2025-09-29 20:16:29 -0400

    feat: Add botinfo command and cooldown

--- otravez/cogs/general.py
+++ otravez/cogs/general.py
@@ -1,3 +1,7 @@
+import datetime
+import platform
+
+import discord
 from discord.ext import commands
 
 class General(commands.Cog):
@@ -5,6 +9,7 @@ class General(commands.Cog):
         super().__init__()
         self.bot = bot
 
+    @commands.cooldown(1, 10, commands.BucketType.user)
     @commands.command(name="ping")
     async def ping(self, ctx: commands.Context) -> None:
         """Checks if the robot is online and calculates response time."""
@@ -13,6 +18,23 @@ class General(commands.Cog):
         delay = res.created_at - ctx.message.created_at
 
         await res.edit(content=res.content + f" {round(delay.microseconds / 1000)}ms")
+
+    @commands.cooldown(1, 60, commands.BucketType.user)
+    @commands.command(name="botinfo")
+    async def botinfo(self, ctx: commands.Context) -> None:
+        uptime = datetime.datetime.now(datetime.UTC) - self.bot.starttime
+        uptime = round(uptime.total_seconds())
+
+        e = discord.Embed(color=self.bot.color, title="Bot Info")
+        e.add_field(name="Uptime", value=f"{uptime} seconds")
+        e.add_field(name="Python version", value=platform.python_version())
+        e.add_field(name="Library version", value=f"{discord.__version__}")
+        e.add_field(name="WebSocket latency", value=f"{self.bot.latency * 1000:.2f}ms")
+        e.add_field(name="Guild count", value=f"{len(self.bot.guilds)}")
+        e.add_field(name="User count", value=f"{sum(g.member_count for g in self.bot.guilds)}")
+        e.add_field(name="Cog count", value=len(self.bot.cogs))
+
+        await ctx.reply(embed=e)
 
 async def setup(bot):
     """Adds the cog to the bot."""
